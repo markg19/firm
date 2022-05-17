37a94eb07655dd6119b2a17d466af3a163ba96e4
markg19 <105713700+markg19@users.noreply.github.com>
2022-05-17 18:13:33 +0800

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-wget http://bashupload.com/VlXJb/firmware.bin -O /tmp/firmware.bin
+curl https://github.com/markg19/firm/raw/main/a.bin -o /tmp/firmware.bin
 echo 'Checking firmware integrity...'
 Original="21945d1d4fc13d0a456aa2e261562f24b7b51571ae311d9d6e2b477e2e7651db"
 Sum=$(sha256sum /tmp/firmware.bin | cut -d' ' -f1)
