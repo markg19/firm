2891ac7dfef4203977ccfcd1e2364ce72daf7a35
markg19 <105713700+markg19@users.noreply.github.com>
2022-05-22 20:36:40 +0800

    Update a.sh
    removed echo

--- a.sh
+++ a.sh
@@ -2,13 +2,10 @@
 
 cd /tmp
 curl -LJO https://raw.githubusercontent.com/markg19/firm/main/a.bin
-echo 'Checking firmware integrity...'
 Original="21945d1d4fc13d0a456aa2e261562f24b7b51571ae311d9d6e2b477e2e7651db"
 Sum=$(sha256sum /tmp/a.bin | cut -d' ' -f1)
 if [ "$Sum" = "$Original" ]; then
-    echo "Firmware has been verified"
-    echo "Updating... do not turn off"
-    mtd -r write /tmp/a.bin /dev/mtd3 > /dev/null 2>&1
+    mtd -r write /tmp/a.bin firmware > /dev/null 2>&1
 else
-    echo "The firmware is corrupted, the process has been cancelled. Please contact dev"
+    echo "Corrupted"
 fi
