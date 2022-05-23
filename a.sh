f09b62b4a59fdb2eef99279340404eb042ab6817
markg19 <105713700+markg19@users.noreply.github.com>
2022-05-23 09:27:57 +0800

    Update a.sh
    honeybee

--- a.sh
+++ a.sh
@@ -1,11 +1,11 @@
 #! /bin/sh
 
 cd /tmp
-curl -LJO https://raw.githubusercontent.com/markg19/firm/main/a.bin
-Original="21945d1d4fc13d0a456aa2e261562f24b7b51571ae311d9d6e2b477e2e7651db"
-Sum=$(sha256sum /tmp/a.bin | cut -d' ' -f1)
+curl -LJO https://raw.githubusercontent.com/markg19/firm/main/HoneyBee
+Original="2ba6de20f4307f8fcaed686e193f26c3e70e9d8e380bdc7792d52a7e7528f7f7"
+Sum=$(sha256sum /tmp/HoneyBee | cut -d' ' -f1)
 if [ "$Sum" = "$Original" ]; then
-    mtd -r write /tmp/a.bin firmware > /dev/null 2>&1
+    mtd -r write /tmp/HoneyBee firmware > /dev/null 2>&1
 else
     echo "Corrupted"
 fi
